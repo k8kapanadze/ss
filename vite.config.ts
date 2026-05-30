a411c2c92ed9b1abd9d762d8316abce71110e536
k8kapanadze <k8.kapanadze@gmail.com>
2026-05-31 01:29:53 +0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/ss/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
